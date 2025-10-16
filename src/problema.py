707656d5b74707b455dc5249f58b8470a7b0ce2e
HectorBernabe <iscjbernabe037@accitesz.com>
2025-10-15 21:22:29 -0600

    completo problema

--- src/problema.py
+++ src/problema.py
@@ -12,3 +12,33 @@ def calcular_fitness(cromosoma):
     for j, fert_nombre in enumerate(LISTA_FERTILIZANTES):
         cantidad_total_fert = np.sum(matriz_solucion[:,j])
         costo_total += cantidad_total_fert * COSTOS[fert_nombre]
+
+    #calculo de penalizaciones
+    for i, cult_nombre in enumerate(CULTIVOS):
+        nutrientes_recibidos = {nut: 0 for nut in NUTRIENTES}
+
+        #calculamos el total de nutrientes para el cultivo i
+        for j, fert_nombre in enumerate(LISTA_FERTILIZANTES):
+            cantidad_aplicada = matriz_solucion[i,j]
+            for nut in NUTRIENTES:
+                nutrientes_recibidos[nut] += cantidad_aplicada * FERTILIZANTES[fert_nombre][nut]
+
+        #aplicamos la penalizacion para el cultivo
+        for nut in NUTRIENTES:
+            #penalizacion por deficiencia parte 2 de la ecuacion
+            requerimiento_nutriente = REQUERIMIENTOS[cult_nombre][nut]
+            if nutrientes_recibidos[nut]< requerimiento_nutriente:
+                faltante = requerimiento_nutriente - nutrientes_recibidos[nut]
+                penalizacion_total += W1_REQUERIMIENTOS * faltante
+
+            #penalizacion por toxicidad
+            toxicicidad_max_nutriente = TOXICIDAD_MAXIMA[nut]
+            if nutrientes_recibidos[nut] > toxicicidad_max_nutriente:
+                exceso = nutrientes_recibidos[nut] - toxicicidad_max_nutriente
+                penalizacion_total +=W2_REQUERIMIENTOS *exceso
+
+    #calculo final
+    costo_final_penalizado = costo_total + penalizacion_total
+
+    #convertimos a fitness 
+    return -costo_final_penalizado
